2437797d5f0200d3bc1cc73b30d0b6596366da8c
guohailong11726 <guohailong11726@ipalfish.com>
2022-04-26 16:27:43 +0800

    feat: add test file

--- creational/builder/builder_test.go
+++ creational/builder/builder_test.go
@@ -1,1 +1,18 @@
 package builder
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBuilder(t *testing.T) {
+	normalBuilder := Builder("normal")
+	normalDirector := NewDirector(normalBuilder)
+	r := normalDirector.BuildHouse()
+	fmt.Println(r)
+
+	iglooBuilder := Builder("igloo")
+	iglooDirector := NewDirector(iglooBuilder)
+	r = iglooDirector.BuildHouse()
+	fmt.Println(r)
+}
