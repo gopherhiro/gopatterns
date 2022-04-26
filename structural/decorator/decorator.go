9164cc5efd61c81a5c3381eee47b0d825bcffd52
guohailong11726 <guohailong11726@ipalfish.com>
2022-04-26 21:39:23 +0800

    feat: decorator

--- structural/decorator/decorator.go
+++ structural/decorator/decorator.go
@@ -1,1 +1,34 @@
 package decorator
+
+// 基本组件和可选层次的通用方法。
+type Pizza interface {
+	price() int
+}
+
+// 创建一个具体组件类， 并定义其基础行为。
+type vegetable struct {
+	p int
+}
+
+func (v *vegetable) price() int {
+	v.p = 15
+	return v.p
+}
+
+// 具体装饰
+type withTomato struct {
+	pizza Pizza
+}
+
+func (t *withTomato) price() int {
+	return t.pizza.price() + 7
+}
+
+// 具体装饰
+type withCheese struct {
+	pizza Pizza
+}
+
+func (c *withCheese) price() int {
+	return c.pizza.price() + 10
+}
--- structural/decorator/decorator_test.go
+++ structural/decorator/decorator_test.go
@@ -1,1 +1,21 @@
 package decorator
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDecorator(t *testing.T) {
+	pizza := &vegetable{}
+	fmt.Println("price of vegetable Pizza:", pizza.price())
+
+	withTomato := &withTomato{
+		pizza: pizza,
+	}
+	fmt.Println("price of vegetable Pizza with tomato:", withTomato.price())
+
+	withCheese := withCheese{
+		pizza: pizza,
+	}
+	fmt.Println("price of vegetable Pizza with tomato and cheese:", withCheese.price())
+}
